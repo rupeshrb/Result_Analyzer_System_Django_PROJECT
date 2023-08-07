1cbb812e5457ecd81569cf235e1bbd19de849ed8
Rupesh Borse <rupeshborse45@gmail.com>
2023-08-07 22:47:52 +0530

    fits commit

---
+++ srmsApp/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from srmsApp import models
+
+# Register your models here.
+admin.site.register(models.Class)
+admin.site.register(models.Subject)
+admin.site.register(models.Student)
+admin.site.register(models.Result)
+admin.site.register(models.Student_Subject_Result)
+admin.site.register(models.user)
